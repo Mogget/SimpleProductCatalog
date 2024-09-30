8f06a145ad76e89a9697474c70fdfe9d01b14d47
Karol Skoruch <karol.skoruch@gmail.com>
2024-10-01 01:09:33 +0200

    Added left sidebar menu Added component(product list and add product

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        children: [
+            {
+              path: '',
+              redirectTo: 'add',
+              pathMatch: 'full',
+            },
+            {
+                path: 'add',
+                loadChildren: () => import('./modules/add-product/add-product.routes').then((routes) => routes.routes),
+            },
+            {
+                path: 'list',
+                loadChildren: () => import('./modules/product-list/product-list.routes').then((routes) => routes.routes),
+            },
+        ]
+    }
+];
---
+++ Frontend/src/app/modules/add-product/add-product.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from "@angular/router";
+import { AddProductComponent } from "./add-product/add-product.component";
+
+export const routes: Routes = [
+    { path: '', component: AddProductComponent }
+]
