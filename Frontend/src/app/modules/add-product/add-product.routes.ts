0be17a33e4fbe0df8ca85675b647db27938c5927
Karol Skoruch <karol.skoruch@gmail.com>
2024-10-02 15:07:16 +0200

    Add link to sidebar, Added form to create product, Form UX experience

--- Frontend/src/app/modules/add-product/add-product.routes.ts
+++ Frontend/src/app/modules/add-product/add-product.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from "@angular/router";
 import { AddProductComponent } from "./add-product/add-product.component";
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 export const routes: Routes = [
-    { path: '', component: AddProductComponent }
+    {
+        path: '', component: AddProductComponent,
+        providers: [
+            {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}}
+        ]
+     }
 ]
--- Frontend/src/app/modules/add-product/add-product/add-product.component.ts
+++ Frontend/src/app/modules/add-product/add-product/add-product.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, FormGroupDirective, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-add-product',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule, 
+    ReactiveFormsModule,
+    MatButton,
+    MatFormField,
+    MatInput,
+    MatLabel,
+  ],
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.scss'
 })
-export class AddProductComponent {
+export class AddProductComponent implements AfterViewInit {
+  @ViewChild("productCodeInput")
+  productCodeInput!: ElementRef;
+  @ViewChild("productFormEl")
+  productForm!: FormGroupDirective;
 
+  ngAfterViewInit() {
+    this.productCodeInput.nativeElement.focus();
+  }
+
+  form = new FormGroup({
+    id: new FormControl(null),
+    code: new FormControl(''),
+    name: new FormControl(''),
+    price: new FormControl(0),
+    description: new FormControl(''),
+    modifiedDate: new FormControl(null)
+  });
+  submitProductForm() {
+    this.form.updateValueAndValidity();
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    const product = this.form.getRawValue();
+    setTimeout(() => this.productForm.resetForm(), 200);
+  }
 }
